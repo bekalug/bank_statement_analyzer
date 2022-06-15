6a556653a80c0977795d8d27c32d3cf97b269246
Bekalu Getnet <bekalugetnet21@gmail.com>
2022-06-15 14:32:18 +0300

    the core functionalities built

---
+++ src/main/java/MainApplication.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+
+public class MainApplication {
+    public static void main(final String... args) throws IOException {
+        final BankStatementAnalyzer bankStatementAnalyzer
+                = new BankStatementAnalyzer();
+
+        final BankStatementParser bankStatementParser
+                = new BankStatementCSVParser();
+
+        bankStatementAnalyzer.analyze("bank_statement.csv", bankStatementParser);
+    }
+}
